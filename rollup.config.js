eed052887af9d24d14bf9f8a74b237f64d03a99e
Murat Yukselen <muratyukselen@gmail.com>
2020-11-04 17:30:07 +0300

    rollup to bundle library

---
+++ rollup.config.js
@@ -0,0 +1,10 @@
+export default {
+  input: 'core/rfb.js',
+  output: {
+    file: 'build/bundle.js',
+    format: 'iife',
+    name: 'NovncRFB',
+    sourcemap: true
+    //sourcemap: 'inline'
+  }
+};
